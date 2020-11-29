720f9b5b88bd26ab43ccf1a9c6e76bfddfea065a
Shih-Chuan Chao <philadelphiafs@gmail.com>
2020-11-29 20:22:54 +0900

    FEATURE - Implement the main class - Implemented the method for set the callback - Extended the constructor

--- TinyRepoSearcher/GithubSearcher/src/main/java/com/scchao/githubsearcher/GitRepoSearcher.java
+++ TinyRepoSearcher/GithubSearcher/src/main/java/com/scchao/githubsearcher/GitRepoSearcher.java
@@ -4,16 +4,27 @@ import android.content.Context;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.scchao.githubsearcher.interfaces.RequestFetchListener;
 
 public class GitRepoSearcher {
     private static final String LOG_TAG = "GitRepoSearcher";
 
     private Context context;
     private RequestQueue requestQueue;
+    private RequestFetchListener callback;
 
     public GitRepoSearcher(Context context) {
         this.context = context;
         requestQueue = Volley.newRequestQueue(context);
+    }
+
+    public GitRepoSearcher(Context context, RequestFetchListener callback) {
+        this(context);
+        setFetchCallback(callback);
+    }
+
+    public void setFetchCallback(RequestFetchListener callback) {
+        this.callback = callback;
     }
 
 }
