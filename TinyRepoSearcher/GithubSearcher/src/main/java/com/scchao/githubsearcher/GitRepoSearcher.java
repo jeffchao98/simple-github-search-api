32c99a943a555750bc9c531254ae1fc4b7a6d1ea
Shih-Chuan Chao <philadelphiafs@gmail.com>
2020-11-29 20:26:49 +0900

    FEATURE - Implement the main class - Added the method for print the fetched items - Added the method for make the StringRequest objects - Implemented the method for execute the url fetch with the given string props

--- TinyRepoSearcher/GithubSearcher/src/main/java/com/scchao/githubsearcher/GitRepoSearcher.java
+++ TinyRepoSearcher/GithubSearcher/src/main/java/com/scchao/githubsearcher/GitRepoSearcher.java
@@ -1,10 +1,19 @@
 package com.scchao.githubsearcher;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.scchao.githubsearcher.helper.DataParser;
 import com.scchao.githubsearcher.interfaces.RequestFetchListener;
+import com.scchao.githubsearcher.models.RepoItem;
+import com.scchao.githubsearcher.models.RepoSearchResult;
+
+import java.util.List;
 
 public class GitRepoSearcher {
     private static final String LOG_TAG = "GitRepoSearcher";
@@ -27,4 +36,40 @@ public class GitRepoSearcher {
         this.callback = callback;
     }
 
+    public void searchWith(String platform, String organization) {
+        String queryUrl = String.format(
+                "https://api.github.com/search/repositories?q=%s+org:%s", platform, organization);
+        requestQueue.add(makeVolleyRequest(queryUrl));
+    }
+
+    private StringRequest makeVolleyRequest(String url) {
+        return new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                RepoSearchResult searchResult = DataParser.parserResult(response);
+                if (callback != null) {
+                    callback.onFetch(true, searchResult.items);
+                }
+                printFetchedItems(searchResult.items);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (callback != null) {
+                    callback.onFetch(false, null);
+                }
+                error.printStackTrace();
+            }
+        });
+    }
+
+    private void printFetchedItems(List<RepoItem> items) {
+        if (items == null) {
+            return;
+        }
+        Log.i(LOG_TAG, String.format("Fetched items count: %d", items.size()));
+        for (RepoItem item : items) {
+            Log.i(LOG_TAG, item.description == null ? "" : item.description);
+        }
+    }
 }
