ab58d42cdb4fb775ec125806b3aff32b8049baef
Shih-Chuan Chao <philadelphiafs@gmail.com>
2020-11-29 20:31:35 +0900

    MISC - Added the logic on MainActivity for test-run on device

--- TinyRepoSearcher/app/src/main/java/com/scchao/tinyreposearcher/MainActivity.java
+++ TinyRepoSearcher/app/src/main/java/com/scchao/tinyreposearcher/MainActivity.java
@@ -3,6 +3,13 @@ package com.scchao.tinyreposearcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.scchao.githubsearcher.GitRepoSearcher;
+import com.scchao.githubsearcher.interfaces.RequestFetchListener;
+import com.scchao.githubsearcher.models.RepoItem;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GitRepoSearcher searcher =  new GitRepoSearcher(this, new RequestFetchListener() {
+            @Override
+            public void onFetch(boolean success, List<RepoItem> items) {
+                Log.i("MainActivity", "success");
+            }
+        });
+        searcher.searchWith("android", "rakutentech");
+        searcher.searchWith("android", "google");
+        searcher.searchWith("adakjdjln", "klklejlkr");
     }
 }
